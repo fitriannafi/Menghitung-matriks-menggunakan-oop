37bed0a8809e9525d244323afa916576eecf79ef
fitriannafi <fitriannafi13@gmail.com>
2020-05-03 11:04:19 +0700

    Add files via upload

---
+++ hitung_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Matriks;
+
+/**
+ *
+ * @author ASUS
+ */
+public class hitung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {           
+        Matriks M = new Matriks();
+        M.Penjumlahan();
+        Matriks2 N = new Matriks2();
+        N.Pengurangan();
+
+    }
+    
+}
